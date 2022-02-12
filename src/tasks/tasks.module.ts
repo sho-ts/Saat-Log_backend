2243d2ed6c758b1d634f047531d548ee8ae43a5e
tagawa shouta <sho.webfe@gmail.com>
2022-02-12 22:48:54 +0900

    :+1: TasksResolverを作成

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.model';
+import { TasksResolver } from './tasks.resolver';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])]
+  imports: [TypeOrmModule.forFeature([Task])],
+  providers: [TasksResolver]
 })
 export class TasksModule { }
