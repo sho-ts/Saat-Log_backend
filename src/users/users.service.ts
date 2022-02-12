eb57b27a07fb796d37c08ab78be4aea5cf969a67
tagawa shouta <sho.webfe@gmail.com>
2022-02-12 22:49:47 +0900

    :+1: createUserを作成

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.model';
+import { UserInput } from './dto/user.input';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
-  ) {}
+  ) { }
 
   findOneById(userId: number): Promise<User> {
     return this.usersRepository.findOne(userId);
+  }
+
+  async create(params: UserInput) {
+    const user = this.usersRepository.create(params);
+    return await this.usersRepository.save(user);
   }
 }
