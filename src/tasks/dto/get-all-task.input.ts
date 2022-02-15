d692fbeaebc330c6cfb74811f351c59add4e7fbd
tagawa shouta <sho.webfe@gmail.com>
2022-02-15 23:30:18 +0900

    :sparkles: pagedをnullable trueに変更

--- src/tasks/dto/get-all-task.input.ts
+++ src/tasks/dto/get-all-task.input.ts
@@ -2,8 +2,8 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class GetAllTaskInput {
-  @Field(type => Int)
-  paged: number;
+  @Field(type => Int, { nullable: true })
+  paged?: number;
 
   @Field()
   userId: string;
