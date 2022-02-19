4192f5106ca806e4968c17af3955256763e0235a
tagawa shouta <sho.webfe@gmail.com>
2022-02-19 22:35:18 +0900

    :+1: read,readAll,deleteを追加

--- src/day-tasks/day-tasks.resolver.ts
+++ src/day-tasks/day-tasks.resolver.ts
@@ -2,23 +2,30 @@ import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { DayTasksService } from './day-tasks.service';
 import { DayTask } from './day-task.model';
 import { AddDayTaskInput } from './dto/add-day-task.input';
+import { GetDayTaskInput } from './dto/get-day-task.input';
+import { GetAllDayTaskInput } from './dto/get-all-day-task.input';
 
 @Resolver()
 export class DayTasksResolver {
   constructor(private dayTasksService: DayTasksService) { }
 
   @Query(returns => DayTask)
-  async getDayTask() {
-
+  async getDayTask(@Args('params') params: GetDayTaskInput) {
+    return await this.dayTasksService.read(params);
   }
 
   @Query(returns => [DayTask])
-  async getAllDayTasks(@Args('userId') userId: string) {
-    return await this.dayTasksService.readAll(userId);
+  async getAllDayTasks(@Args('params') params: GetAllDayTaskInput) {
+    return await this.dayTasksService.readAll(params);
   }
 
   @Mutation(returns => DayTask)
   async addDayTask(@Args('params') params: AddDayTaskInput) {
     return await this.dayTasksService.create(params);
+  }
+
+  @Mutation(returns => Boolean)
+  async deleteDayTask(@Args('params') params: GetDayTaskInput) {
+    return await this.dayTasksService.delete(params);
   }
 }
