48034a9c6cc672fb9429de9717bce34a34c128f4
tagawa shouta <sho.webfe@gmail.com>
2022-03-05 17:41:21 +0900

    :notebook: 依存性注入

--- src/day-tasks/day-tasks.module.ts
+++ src/day-tasks/day-tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DayTask } from './day-task.model';
+import { Task } from '../tasks/task.model';
 import { DayTasksResolver } from './day-tasks.resolver';
 import { DayTasksService } from './day-tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DayTask])],
-  providers: [DayTasksResolver, DayTasksService]
+  imports: [TypeOrmModule.forFeature([DayTask, Task])],
+  providers: [DayTasksResolver, DayTasksService],
 })
-export class DayTasksModule { }
+export class DayTasksModule {}
